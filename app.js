1058de69f01006e151c0ca6ab2ee83aaa63e1cc5
[Notanewbiecoder] <[oladokspelz@gmail,com]>
2021-01-30 08:46:33 +0100

    addposts

--- app.js
+++ app.js
@@ -30,26 +30,157 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/assets", express.static(__dirname + "/public"));
 
+//app.get("/html/categories.html", (req, res) => {
+//let sql = "SELECT * FROM realcategory";
+//let query = conn.query(sql, (err, results) => {
+// if (err) throw err;
+//res.render("categories", {
+// results: results,
+// });
+//});
+//});
+
+//app.post("/save", (req, res) => {
+//let data = {
+//Title: req.body.Title,
+//Date: Date.now,
+//};
+//console.log(data);
+//let sql = "INSERT INTO realcategory SET ?";
+//let query = conn.query(sql, data, (err, results) => {
+//if (err) throw err;
+//res.redirect("/html/categories.html");
+//});
+//});
+
 app.get("/", (req, res) => {
-  let sql = "SELECT * FROM realcategory";
+  let sql = "SELECT * FROM addposts";
   let query = conn.query(sql, (err, results) => {
     if (err) throw err;
-    res.render("categories", {
+    res.render("posts", {
       results: results,
     });
   });
 });
 
 app.post("/save", (req, res) => {
-  let data = { Title: req.body.Title, Date: req.body.Date };
-  console.log(data);
-  let sql = "INSERT INTO realcategory (Title, Date) VALUES(Nokia, 2021-01-25)";
-  let query = conn.query(sql, data, (err, results) => {
+  let poster = {
+    Title: req.body.Title,
+    Category: req.body.Category,
+    Date: Date.now,
+  };
+  console.log(poster);
+  let sql = "INSERT INTO addposts SET ?";
+  let query = conn.query(sql, poster, (err, results) => {
     if (err) throw err;
+    res.redirect("/");
   });
-  console.log(query);
 });
 
 app.listen(5000, () => {
   console.log("Server is running at port 5000");
+});
+
+app.get("/", (req, res) => {
+  res.render("blogpost");
+});
+
+app.get("/details.html", (req, res) => {
+  res.render("details");
+});
+
+app.get("/login.html", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register.hbs", (req, res) => {
+  res.render("register");
+});
+
+app.get("/posts.html", (req, res) => {
+  res.render("posts");
+});
+
+app.get("/categories.html", (req, res) => {
+  res.render("categories");
+});
+
+app.get("/users.html", (req, res) => {
+  res.render("users");
+});
+
+app.get("/html/Adminindex.html", (req, res) => {
+  res.render("Adminindex");
+});
+
+app.get("/html/users.html", (req, res) => {
+  res.render("users");
+});
+
+app.get("/html/categories.html", (req, res) => {
+  res.render("categories");
+});
+
+app.get("/html/posts.html", (req, res) => {
+  res.render("posts");
+});
+
+app.get("/html/login.html", (req, res) => {
+  res.render("login");
+});
+
+app.get("/html/profile.html", (req, res) => {
+  res.render("profile");
+});
+
+app.get("/html/settings.html", (req, res) => {
+  res.render("settings");
+});
+
+app.get("/html/blogpost.hbs", (req, res) => {
+  res.render("blogpost");
+});
+
+app.get("/views/blogpost.hbs", (req, res) => {
+  res.render("blogpost");
+});
+
+app.get("/views/single.hbs", (req, res) => {
+  res.render("single");
+});
+
+app.get("/views/Adminindex.html", (req, res) => {
+  res.render("Adminindex");
+});
+
+app.get("/views/posts.html", (req, res) => {
+  res.render("posts");
+});
+
+app.get("/views/categories.html", (req, res) => {
+  res.render("categories");
+});
+
+app.get("/views/users.html", (req, res) => {
+  res.render("users");
+});
+
+app.get("/views/register.hbs", (req, res) => {
+  res.render("register");
+});
+
+app.get("/views/login.html", (req, res) => {
+  res.render("login");
+});
+
+app.get("/html/categories.hbs", (req, res) => {
+  res.render("categories");
+});
+
+app.get("/html/users.html", (req, res) => {
+  res.render("users");
+});
+
+app.get("/html/register.hbs", (req, res) => {
+  res.render("register");
 });
